52ea27528c771a4f52c578550405da81bf3d278f
vitor-creat <vitorluciopadilhaoliveira4@gmail.com>
2026-05-15 16:14:37 -0400

    botão de salvar feito.

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,18 @@ function App() {
     const handleClearClick = ()=>{
     canvasRef.current?.clearCanvas()
   }
+  const handleExportImage = async () =>{
+    const x = await canvasRef.current?.exportImage('png')
+    //o x é uma constante que contém a uri do que foi desenhado
+    //a uri tem o seguinte formato: data:image/png;base64
+    //vou enviar essa uri para o back-end e com o python, vou transformar ela em uma image pillow com o base64
+    console.log(x)
+  }
   return (
     <>
       <div id="center">
               <h1>Draw a Digit</h1>
-        <ReactSketchCanvas ref={canvasRef} width='800' height='700' allowOnlyPointerType='mouse'/>
+        <ReactSketchCanvas ref={canvasRef} width='800' height='700' allowOnlyPointerType='mouse' strokeColor='black'/>
       
         <button
           type="button"
@@ -29,6 +36,13 @@ function App() {
           Clear
         </button>
 
+        <button
+          type="button"
+          onClick={handleExportImage}
+
+        >
+          save
+        </button>
       </div>
 
     </>
